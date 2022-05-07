8c2b3009e983a34e09ec1788d58d920ee656862e
tnaahsus <72088338+tnaahsus@users.noreply.github.com>
2022-05-07 19:38:12 +0530

    Solved router error

--- frontend/blog-section/src/components/reader.js
+++ frontend/blog-section/src/components/reader.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import ErrorPage from "./Errorpage";
 import Loading from "./loading";
